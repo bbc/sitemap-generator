9aedcbf1524c3b249e69f3721d121a8e5517f90d
Adam Roberts <adam.roberts@bbc.co.uk>
2020-04-09 12:05:35 +0100

    revert bitesize changes

--- src/SitemapStream.js
+++ src/SitemapStream.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const escapeUnsafe = require('./helpers/escapeUnsafe');
 
 module.exports = function SitemapStream() {
+
   const tmpPath = path.join(os.tmpdir(), `sitemap_${rand(10)}`);
   const stream = fs.createWriteStream(tmpPath);
 
@@ -13,19 +14,9 @@ module.exports = function SitemapStream() {
     '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
   );
 
-  const dailyFreq = [
-    'https://www.bbc.co.uk/bitesize',
-    'https://www.bbc.co.uk/bitesize/learn',
-    'https://www.bbc.co.uk/bitesize/support',
-    'https://www.bbc.co.uk/bitesize/careers'
-  ];
-
   const getPath = () => tmpPath;
 
   const write = (url, currentDateTime, changeFreq, priority) => {
-    if (dailyFreq.includes(url) || url.includes('collections') || url.includes('tags') || url.includes('dailylessons')) {
-      changeFreq = 'daily';
-    }
     const escapedUrl = escapeUnsafe(url);
     stream.write('\n  <url>\n');
     stream.write(`    <loc>${escapedUrl}</loc>\n`);
@@ -49,6 +40,6 @@ module.exports = function SitemapStream() {
   return {
     getPath,
     write,
-    end
+    end,
   };
 };
